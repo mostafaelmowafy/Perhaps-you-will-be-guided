b0176ed6c3410d6559fed615a749e98703f3dbb6
mostafaelmowafy <mostafaelmowafy1@gmail.com>
2024-03-16 00:47:25 +0200

    change in data and question page

--- src/Questions.js
+++ src/Questions.js
@@ -5,7 +5,11 @@ import { questionsData } from "./Data";
 export default function Question() {
   const [step, setStep] = useState(1);
   const [answerIsOpen, setAnswerIsOpen] = useState(false);
-
+  const [show, setShow] = useState("prophets");
+  const singleData = questionsData.filter((q) => {
+    return q.name === show;
+  })[0].data;
+  console.log(singleData);
   function preHandler() {
     if (step > 1) {
       setStep((s) => --s);
@@ -28,52 +32,88 @@ export default function Question() {
   //     q = { ...q, active: true };
   //   }
   return (
-    <div className={styles.container}>
-      <div className={styles.steps}>
-        <div className={styles.numbers}>
-          {questionsData.map((q) => {
-            return (
-              <>
-                <div
-                  key={q.id}
-                  onClick={() => {
-                    setStep(q.id);
-                    q.active = true;
-                  }}
-                  className={q.active && styles.active}
-                >
-                  {q.id}
-                </div>
-              </>
-            );
-          })}
-        </div>
-        <div className={styles.message}>
-          <p>
-            {step}: {questionsData[step - 1].qes}
-          </p>
-        </div>
-        <div className={styles.buttons}>
-          <Button onClick={preHandler}>
-            <span>⏮️</span>السابق
-          </Button>
-          <button
-            onClick={showHandler}
-            className={answerIsOpen && styles.active}
+    <>
+      <div className={styles.header}>
+        <ul>
+          <li
+            onClick={() => {
+              setShow("prophets");
+            }}
           >
-            الأجابة
-          </button>
-          <Button onClick={nextHandler}>
-            التالي<span>⏭️</span>
-          </Button>
-        </div>
-        {answerIsOpen && (
-          <div className={styles.answer}>
-            <h3 className={styles.ques_h3}>{questionsData[step - 1].answer}</h3>
-          </div>
-        )}
+            الأنبياء
+          </li>
+          <li
+            onClick={() => {
+              setShow("history");
+            }}
+          >
+            تاريخ
+          </li>
+          <li
+            onClick={() => {
+              setShow("jurisprudence");
+            }}
+          >
+            فقه
+          </li>
+          <li
+            onClick={() => {
+              setShow("quran");
+            }}
+          >
+            قران
+          </li>
+        </ul>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.steps}>
+          <div className={styles.numbers}>
+            {singleData.map((q) => {
+              return (
+                <>
+                  <div
+                    key={q.id}
+                    onClick={() => {
+                      setStep(q.id);
+                      q.active = true;
+                    }}
+                    className={q.active && styles.active}
+                  >
+                    {q.id}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          <div className={styles.message}>
+            <p>
+              {step}: {questionsData[step - 1].qes}
+            </p>
+          </div>
+          <div className={styles.buttons}>
+            <Button onClick={preHandler}>
+              <span>⏮️</span>السابق
+            </Button>
+            <button
+              onClick={showHandler}
+              className={answerIsOpen && styles.active}
+            >
+              الأجابة
+            </button>
+            <Button onClick={nextHandler}>
+              التالي<span>⏭️</span>
+            </Button>
+          </div>
+          {answerIsOpen && (
+            <div className={styles.answer}>
+              <h3 className={styles.ques_h3}>
+                {questionsData[step - 1].answer}
+              </h3>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 function Button({ onClick, children }) {
